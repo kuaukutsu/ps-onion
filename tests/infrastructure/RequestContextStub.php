bb57218f89770a4731afcac4e26343ac11e9c01e
Dmitriy Krivopalov <dimarik82@gmail.com>
2024-10-20 14:00:18 +0300

    add tests

---
+++ tests/infrastructure/RequestContextStub.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace kuaukutsu\ps\onion\tests\infrastructure;
+
+use Override;
+use kuaukutsu\ps\onion\domain\interface\RequestContext;
+
+final readonly class RequestContextStub implements RequestContext
+{
+    /**
+     * @param non-empty-string $uuid
+     */
+    public function __construct(private string $uuid)
+    {
+    }
+
+    #[Override]
+    public function getUuid(): string
+    {
+        return $this->uuid;
+    }
+}
