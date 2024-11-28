b82b3c7afe56b0b7503cbbcc5008dd3acd7ae856
Dmitriy Krivopalov <dimarik82@gmail.com>
2024-11-28 17:48:22 +0300

    fix style

--- src/infrastructure/repository/author/AuthorMapper.php
+++ src/infrastructure/repository/author/AuthorMapper.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace kuaukutsu\ps\onion\infrastructure\repository\author;
 
-use LogicException;
 use kuaukutsu\ps\onion\domain\entity\author\Author;
 use kuaukutsu\ps\onion\domain\entity\author\AuthorDto;
 use kuaukutsu\ps\onion\domain\entity\author\AuthorMetadata;
@@ -13,13 +12,6 @@ use kuaukutsu\ps\onion\domain\entity\author\AuthorUuid;
 
 final readonly class AuthorMapper
 {
-    public function __construct()
-    {
-    }
-
-    /**
-     * @throws LogicException is input data not valid
-     */
     public function fromDto(AuthorDto $dto): Author
     {
         return new Author(
